4326ee5ab96f61e463e0a46bb60c83320eead451
linghong <linghong_chen@hotmail.com>
2017-11-24 12:48:01 -0500

    reversestring.

---
+++ 1-reversestring/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+/*function reverse(str) {
+	let newStr = '';
+	for (let char of str ){
+		newStr = char + newStr;
+	}
+	return newStr;
+}*/
+
+/*function reverse(str){
+	return str
+			.split('')
+			.reverse('')
+			.join('');
+}*/
+
+function reverse(str) {
+	return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+module.exports = reverse;
