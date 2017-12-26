e0b15fbb34b05cda47a72be384ff347fa5ab4074
linghong <linghong_chen@hotmail.com>
2017-12-25 23:01:03 -0500

    chunk

---
+++ 6-chunk/index.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+	let i = 0;
+	let chunksArr = [];
+	while ( i < array.length ){
+		let arr =[];
+		for ( let j = 0; j < size; j++){
+			arr.push(array[i]);
+			i++;
+			if (i >= array.length) break;
+		}
+		chunksArr.push(arr);
+	}
+	return chunksArr;
+} 
+
+module.exports = chunk;
