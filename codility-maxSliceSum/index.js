647467cdea3c593bb82ff7a481779465ab3014e7
linghong <linghong_chen@hotmail.com>
2017-12-28 23:25:41 -0500

    Max Slice Sum

--- codility-maxSliceSum/index.js
+++ codility-maxSliceSum/index.js
@@ -1,11 +1,14 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-function solution( A) {
+function solution( A) {	
     let maxEndingHere = A[0];
     let maxSoFar = A[0];
     for(let i = 1; i < A.length; i++){
+    	//maxEndingHere >= 0, maxEndingHere = maxEndingHere+A[i];
+		//when maxEndingHere < 0, maxEndingHere = A[i];
         maxEndingHere = Math.max(A[i], maxEndingHere + A[i]);
+        //if the new slice is larger, switch to new slice
         maxSoFar = Math.max(maxSoFar, maxEndingHere);
     }
     return maxSoFar;
