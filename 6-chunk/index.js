240f0584143b19d9f01cda0550cf74dc74aa5d3d
linghong <linghong_chen@hotmail.com>
2017-12-30 18:46:43 -0500

    chunk

--- 6-chunk/index.js
+++ 6-chunk/index.js
@@ -8,6 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+/*
+function chunk(array, size){
+	const chunksArr =[];
+	let i = 0;
+	while (i<array.length){
+		chunksArr.push(array.slice(i, i+size));
+		i +=size;
+	}
+	return chunksArr;	
+}*/
+
 function chunk(array, size) {
 	let i = 0;
 	let chunksArr = [];
