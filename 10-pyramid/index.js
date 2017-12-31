81eeb84d9645be8ef1cf5f04868c641aea42f778
linghong <linghong_chen@hotmail.com>
2017-12-31 17:52:23 -0500

    added pyramid.

--- 10-pyramid/index.js
+++ 10-pyramid/index.js
@@ -14,6 +14,18 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+	for (let row = 0; row < n; row++){
+		let str = "";
+		for (let column = 0; column < 2*n-1; column++){
+			if(let column <= n-row||column > n+row){
+				str += " ";
+			} else {
+				str += "#";
+			}
+		}
+		return str;
+	}
+}
 
 module.exports = pyramid;
