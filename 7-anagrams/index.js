a716286789749a68aee54a56bbbae94528bbf256
linghong <linghong_chen@hotmail.com>
2017-12-31 08:25:14 -0500

    anagrams

--- 7-anagrams/index.js
+++ 7-anagrams/index.js
@@ -8,6 +8,8 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+
+//Method1
 function anagrams(stringA, stringB) {
 	const aChars = buildCharMap(stringA);
 	const bChars = buildCharMap(stringB);
@@ -31,4 +33,12 @@ function buildCharMap(str){
 	return charMap;
 }
 
+//Method2
+/*function anagrams(stringA, stringB){
+	return cleanString(stringA)===cleanString(stringB);
+}
+
+function cleanString(str){
+	return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}*/
 module.exports = anagrams;
